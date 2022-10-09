37849066b8e121e8db7186495341013557037828
Annaivv <annashalya@gmail.com>
2022-10-09 18:34:36 +0300

    task 2 throttle added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('timeupdate', e => {
+player.on('timeupdate', throttle(setTime, 1000));
+
+function setTime(e) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
-});
+}
 
 player.setCurrentTime(
   JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0
