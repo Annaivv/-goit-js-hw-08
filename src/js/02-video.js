94e85959f41e05a403113d5a46a22bdea4bcc9e7
Annaivv <annashalya@gmail.com>
2022-10-09 17:56:59 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', e => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+});
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0
+);
