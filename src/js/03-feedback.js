d47060efe665ec30825e97845dcf0f75f6c51339
Annaivv <annashalya@gmail.com>
2022-10-11 10:25:28 +0300

    task-3 correct

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,10 +3,10 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('form');
-const email = document.getElementsByName('input[name = email]');
-const textarea = document.getElementsByName('textarea[name = message]');
+const email = document.querySelector('input[name = email]');
+const textarea = document.querySelector('textarea[name = message]');
 
-const formData = {};
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onFormSubmit);
@@ -22,13 +22,14 @@ function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
 
 function populateForm() {
   const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   console.log(parsedData);
   if (parsedData) {
-    email.value = parsedData.email;
-    textarea.value = parsedData.message;
+    email.value = parsedData.email || '';
+    textarea.value = parsedData.message || '';
   }
 }
